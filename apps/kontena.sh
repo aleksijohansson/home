8ac265381291dc264dc1cf3f9ca99864d0fde20d
Aleksi Johansson <aleksi.johansson@wunderkraut.com>
2016-10-14 09:26:36 +0300

    Changed kontena.sh to use host network so it can access locally hosted master

--- apps/kontena.sh
+++ apps/kontena.sh
@@ -9,11 +9,13 @@ then
   docker run --rm -ti \
   --env=SSL_IGNORE_ERRORS=true \
   --volume=$HOME/.local/share/kontena/home:/home/kontena \
+  --network="host" \
   kontena/cli:latest $@
 elif [ $OS = "Darwin" ]
 then
   docker run --rm -ti \
   --env=SSL_IGNORE_ERRORS=true \
   --volume=$HOME/Library/Application\ Support/kontena/home:/home/kontena \
+  --network="host" \
   kontena/cli:latest $@
 fi
