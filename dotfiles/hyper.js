4fbdca422153984edcca4d7626eb3fc1e518befe
Aleksi Johansson <aleksi.johansson@wunder.io>
2016-11-14 22:36:33 +0200

    Add matching theme with Atom.

--- dotfiles/hyper.js
+++ dotfiles/hyper.js
@@ -82,7 +82,9 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [
+    'hyperterm-base16-tomorrow-dark'
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
