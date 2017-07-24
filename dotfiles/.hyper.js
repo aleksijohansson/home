e17192aa71347ec9115c4a0328219a10195e12cf
Aleksi Johansson <aleksi@aleksijohansson.net>
2017-07-24 16:25:00 +0300

    Removed the Hyper theme plugin and adjusted colors manually accordingly.

--- dotfiles/.hyper.js
+++ dotfiles/.hyper.js
@@ -1,73 +1,71 @@
 module.exports = {
   config: {
-    // default font size in pixels for all tabs
+    // Default font size in pixels for all tabs.
     fontSize: 14,
 
-    // font family with optional fallbacks
+    // Font family with optional fallbacks.
     fontFamily: 'Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
 
-    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: 'rgba(248,28,229,0.8)',
+    // Terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk).
+    cursorColor: '#b294bb',
 
-    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
+    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █.
     cursorShape: 'BLOCK',
 
-    // color of the text
-    foregroundColor: '#fff',
+    // Color of the text.
+    foregroundColor: '#c5c8c6',
 
-    // terminal background color
-    backgroundColor: '#000',
+    // Terminal background color.
+    backgroundColor: '#1d1f21',
 
-    // border color (window, tabs)
-    borderColor: '#333',
+    // Border color (window, tabs).
+    borderColor: '#161719',
 
-    // custom css to embed in the main window
+    // Custom css to embed in the main window.
     css: '',
 
-    // custom css to embed in the terminal window
+    // Custom css to embed in the terminal window.
     termCSS: '',
 
-    // custom padding (css format, i.e.: `top right bottom left`)
+    // Custom padding (css format, i.e.: `top right bottom left`).
     padding: '12px 14px',
     // padding: '0px',
 
-    // the full list. if you're going to provide the full color palette,
-    // including the 6 x 6 color cubes and the grayscale map, just provide
-    // an array here instead of a color map object
+    // Colors from https://github.com/atom/base16-tomorrow-dark-theme/blob/master/styles/colors.less
     colors: {
-      black: '#000000',
-      red: '#ff0000',
-      green: '#33ff00',
-      yellow: '#ffff00',
-      blue: '#0066ff',
-      magenta: '#cc00ff',
-      cyan: '#00ffff',
-      white: '#d0d0d0',
-      lightBlack: '#808080',
-      lightRed: '#ff0000',
-      lightGreen: '#33ff00',
-      lightYellow: '#ffff00',
-      lightBlue: '#0066ff',
-      lightMagenta: '#cc00ff',
-      lightCyan: '#00ffff',
-      lightWhite: '#ffffff'
+      black: '#1d1f21', // black
+      red: '#cc6666', // red
+      green: '#b5b84d', // green
+      yellow: '#f0c674', // yellow
+      blue: '#81a2be', // blue
+      magenta: '#b294bb', // purple
+      cyan: '#8abeb7', // cyan
+      white: '#e0e0e0', // almost-white
+      lightBlack: '#282a2e', // very-dark-gray
+      lightRed: '#cc6666', // red
+      lightGreen: '#b5bd68', // green
+      lightYellow: '#f0c674', // yellow
+      lightBlue: '#81a2be', // blue
+      lightMagenta: '#b294bb', // purple
+      lightCyan: '#8abeb7', // cyan
+      lightWhite: '#ffffff' // white
     },
 
-    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-    // if left empty, your system's login shell will be used by default
+    // The shell to run when spawning a new session (i.e. /usr/local/bin/fish)
+    // if left empty, your system's login shell will be used by default.
     shell: '',
 
-    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
-    // by default ['--login'] will be used
+    // For setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
+    // by default ['--login'] will be used.
     shellArgs: ['--login'],
 
-    // for environment variables
+    // For environment variables.
     env: {},
 
-    // set to false for no bell
+    // Set to false for no bell.
     bell: 'SOUND',
 
-    // if true, selected text will automatically be copied to the clipboard
+    // If true, selected text will automatically be copied to the clipboard.
     copyOnSelect: false
 
     // URL to custom bell
@@ -76,18 +74,16 @@ module.exports = {
     // for advanced config flags please refer to https://hyper.is/#cfg
   },
 
-  // a list of plugins to fetch and install from npm
+  // A list of plugins to fetch and install from npm
   // format: [@org/]project[#version]
   // examples:
   //   `hyperpower`
   //   `@company/project`
-  //   `project#1.0.1`
-  plugins: [
-    'hyperterm-base16-tomorrow-dark'
-  ],
+  //   `project#1.0.1`.
+  plugins: [],
 
-  // in development, you can create a directory under
+  // In development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
-  // to load it and avoid it being `npm install`ed
+  // to load it and avoid it being `npm install`ed.
   localPlugins: []
 };
