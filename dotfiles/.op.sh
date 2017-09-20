f93a6720569093a35b89a1e2fde445c93382e9cc
Aleksi Johansson <aleksi@aleksijohansson.net>
2017-09-21 00:50:34 +0300

    Detect if we ask for ansible vault password from vagrant.

--- dotfiles/.op.sh
+++ dotfiles/.op.sh
@@ -3,13 +3,22 @@
 # Get the Ansible Vault password from 1Password. This assumes the password is
 # saved as a "Secure Notes" type.
 
-# Login if needed.
-if [ -z ${OP_SESSION_my+x} ]; then
-  # Get the secret key from local file.
-  SECRET=`cat ~/.op-secret`
-  eval $(op signin my.1password.com aleksi.johansson@icloud.com $SECRET)
+# Only ask for the password file if not run by Vagrant.
+# Vagrant is not able to ask for the password.
+if [ -z ${VAGRANT_INSTALLER_ENV+x} ]; then
+  # Login if needed.
+  if [ -z ${OP_SESSION_my+x} ]; then
+    # Get the secret key from local file.
+    SECRET=`cat $HOME/.op-secret`
+    eval $(op signin my.1password.com aleksi.johansson@icloud.com "$SECRET")
+  fi
+  # Get the password with the name of the current project folder.
+  PROJECT=`basename $PWD`
+  op get item "$PROJECT" | jq '.details.notesPlain' --raw-output
+else
+  >&2 echo "
+Vagrant is running ansible-playbook so we will get the vault
+password from WT_ANSIBLE_VAULT_FILE environment variable.
+"
+  cat "$WT_ANSIBLE_VAULT_FILE"
 fi
-
-# Get the password with the name of the current project folder.
-PROJECT=`basename $PWD`
-op get item $PROJECT | jq '.details.notesPlain' --raw-output
