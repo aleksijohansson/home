9095c6bfe5cb1df9d171a5ad58dd38b1b367176f
Aleksi Johansson <aleksi@aleksijohansson.net>
2018-02-18 17:41:12 +0200

    Added plugin to Hyper for rearranging tabs.

--- dotfiles/.hyper.js
+++ dotfiles/.hyper.js
@@ -80,7 +80,9 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`.
-  plugins: [],
+  plugins: [
+    'hyperterm-tabs'
+  ],
 
   // In development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
