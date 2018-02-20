5f68eca376551f090d3340ca4fdb128f065712e9
Aleksi Johansson <aleksi@aleksijohansson.net>
2018-02-20 20:08:18 +0200

    Atom command is recognized without an alias.

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -5,7 +5,3 @@ New-Alias -Name git -Value "$Env:ProgramFiles\Git\bin\git.exe"
 # Alias Vim
 #
 New-Alias -Name vim -Value "$Env:ProgramFiles (x86)\Vim\vim80\vim.exe"
-
-# Alias Atom
-#
-New-Alias -Name atom -Value "$Env:LOCALAPPDATA\atom\bin\atom"
