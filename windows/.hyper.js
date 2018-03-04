dec2e9959d1a40d249af4443d14e44c2b3ecc0eb
Aleksi Johansson <aleksi@aleksijohansson.net>
2018-03-04 12:23:46 +0200

    Trying to use the Hyper canary on Windows, hyperterm-tabs fails still.

--- windows/.hyper.js
+++ windows/.hyper.js
@@ -6,7 +6,7 @@ module.exports = {
   config: {
     // Choose either "stable" for receiving highly polished,
     // or "canary" for less polished but more frequent updates
-    updateChannel: 'stable',
+    updateChannel: 'canary',
 
     // default font size in pixels for all tabs
     fontSize: 14,
@@ -122,7 +122,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyperterm-tabs", "hyperterm-tab-icons"],
+  plugins: ["hyperterm-tabs#next", "hyperterm-tab-icons"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
